c98413a9a778b7b6ca93fc36ac657bec6030f9d0
andreid27 <andreid@globalsoftsys.ro>
2023-08-16 22:29:00 +0300

    upload FE working

--- src/views/forms/form-elements/file-uploader/FileUploaderSingle.js
+++ src/views/forms/form-elements/file-uploader/FileUploaderSingle.js
@@ -1,21 +1,12 @@
-// ** React Imports
 import { useState } from 'react'
-
-// ** MUI Imports
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-
-// ** Icon Imports
 import Icon from 'src/@core/components/icon'
-
-// ** Third Party Imports
 import { useDropzone } from 'react-dropzone'
 
 const FileUploaderSingle = () => {
-  // ** State
   const [files, setFiles] = useState([])
 
-  // ** Hooks
   const { getRootProps, getInputProps } = useDropzone({
     multiple: false,
     accept: {
@@ -27,37 +18,50 @@ const FileUploaderSingle = () => {
   })
 
   const img = files.map(file => (
-    <img key={file.name} alt={file.name} className='single-file-image' src={URL.createObjectURL(file)} />
+    <img
+      key={file.name}
+      alt={file.name}
+      className='single-file-image'
+      src={URL.createObjectURL(file)}
+      style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+    />
   ))
 
   return (
-    <Box {...getRootProps({ className: 'dropzone' })} sx={files.length ? { height: 450 } : {}}>
+    <Box
+      {...getRootProps({ className: 'dropzone' })}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100%'
+      }}
+    >
       <input {...getInputProps()} />
       {files.length ? (
         img
       ) : (
-        <Box sx={{ display: 'flex', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
-          <Box
-            sx={{
-              mb: 8.75,
-              width: 48,
-              height: 48,
-              display: 'flex',
-              borderRadius: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-              backgroundColor: theme => `rgba(${theme.palette.customColors.main}, 0.08)`
-            }}
-          >
-            <Icon icon='tabler:upload' fontSize='1.75rem' />
-          </Box>
-          <Typography variant='h4' sx={{ mb: 2.5 }}>
-            Drop files here or click to upload.
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            (This is just a demo drop zone. Selected files are not actually uploaded.)
-          </Typography>
+        <Box
+          sx={{
+            width: 200, // Set a fixed width
+            p: 3,
+            borderRadius: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: theme => `rgba(${theme.palette.customColors.main}, 0.08)`
+          }}
+        >
+          <Icon icon='tabler:upload' fontSize='1.75rem' />
         </Box>
+      )}
+      {files.length ? null : (
+        <>
+          <Typography variant='h5' sx={{ pt: 8 }}>
+            Trage fișierul aici sau click pentru upload.
+          </Typography>
+        </>
       )}
     </Box>
   )
