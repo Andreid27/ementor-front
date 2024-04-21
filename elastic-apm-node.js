87da689be77b395c496cf68193e722944a9fa7bb
Andrei Dinca <andrei@dinca.co>
2024-04-21 16:42:32 +0300

    change default env to prod

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -2,5 +2,5 @@ module.exports = {
   serverUrl: 'http://api.e-mentor.ro/apm',
   secretToken: '',
   serviceName: 'frotend-service',
-  environment: 'e-mentor-dev'
+  environment: 'e-mentor-prod'
 }
