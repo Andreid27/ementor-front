90cb690b8208df7f54e98e6812170a45e54d8c2a
Andrei Dinca <andrei@dinca.co>
2024-04-21 16:13:32 +0300

    change env to module for apm

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -1,5 +1,5 @@
 module.exports = {
-  serverUrl: 'https://dev.api.e-mentor.ro/apm',
+  serverUrl: 'http://api.e-mentor.ro/apm',
   secretToken: '',
   serviceName: 'frotend-service',
   environment: 'e-mentor-dev'
